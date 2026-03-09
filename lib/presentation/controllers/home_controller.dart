8ada1bb5c15fc4f120f2a23c85f58be1a09aa868
Roland Godet <rgodet@raida.fr>
2026-03-09 19:26:51 +0100

    refactor(home): prepare controller for use case injection

--- lib/presentation/controllers/home_controller.dart
+++ lib/presentation/controllers/home_controller.dart
@@ -1,3 +1,7 @@
 import 'package:get/get.dart';
 
-class HomeController extends GetxController {}
+class HomeController extends GetxController {
+  HomeController();
+  // Use cases will be injected here via constructor parameters
+  // e.g. HomeController(this._authenticateUser);
+}
