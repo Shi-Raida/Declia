96e6ab78b697c979061fa91c515f98274370ab86
Roland Godet <rgodet@raida.fr>
2026-03-14 14:36:16 +0100

    feat(config): add environment field to AppConfig

--- lib/infrastructure/config/app_config.dart
+++ lib/infrastructure/config/app_config.dart
@@ -1,6 +1,13 @@
+import '../../core/enums/environment.dart';
+
 final class AppConfig {
-  const AppConfig({required this.supabaseUrl, required this.supabaseAnonKey});
+  const AppConfig({
+    required this.supabaseUrl,
+    required this.supabaseAnonKey,
+    required this.environment,
+  });
 
   final String supabaseUrl;
   final String supabaseAnonKey;
+  final Environment environment;
 }
