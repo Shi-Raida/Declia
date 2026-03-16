417d01b734ea6e5d1c586bf262ea32e60e6e649e
Roland Godet <rgodet@raida.fr>
2026-03-16 11:49:29 +0100

    refactor(admin): convert AdminLayout to StatelessWidget

--- lib/presentation/widgets/admin/admin_layout.dart
+++ lib/presentation/widgets/admin/admin_layout.dart
@@ -1,34 +1,15 @@
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
 
-import '../../controllers/admin_shell_controller.dart';
 import '../../theme/app_breakpoints.dart';
 import '../../theme/app_colors.dart';
 import 'admin_sidebar.dart';
 import 'admin_topbar.dart';
 
-class AdminLayout extends StatefulWidget {
+class AdminLayout extends StatelessWidget {
   const AdminLayout({super.key, required this.body, this.title = ''});
 
   final Widget body;
   final String title;
-
-  @override
-  State<AdminLayout> createState() => _AdminLayoutState();
-}
-
-class _AdminLayoutState extends State<AdminLayout> {
-  @override
-  void initState() {
-    super.initState();
-    WidgetsBinding.instance.addPostFrameCallback((_) {
-      final ctrl = Get.find<AdminShellController>();
-      final route = Get.currentRoute;
-      if (ctrl.currentRoute.value != route) {
-        ctrl.currentRoute.value = route;
-      }
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -41,8 +22,8 @@ class _AdminLayoutState extends State<AdminLayout> {
         drawer: const AdminSidebar(),
         body: Column(
           children: [
-            AdminTopbar(title: widget.title, showMenuButton: true),
-            Expanded(child: widget.body),
+            AdminTopbar(title: title, showMenuButton: true),
+            Expanded(child: body),
           ],
         ),
       );
@@ -56,8 +37,8 @@ class _AdminLayoutState extends State<AdminLayout> {
           Expanded(
             child: Column(
               children: [
-                AdminTopbar(title: widget.title, showMenuButton: false),
-                Expanded(child: widget.body),
+                AdminTopbar(title: title, showMenuButton: false),
+                Expanded(child: body),
               ],
             ),
           ),
