f1bd7a1d989551d8a649e58e67fd752b3c814f11
Roland Godet <rgodet@raida.fr>
2026-03-18 09:14:39 +0100

    fix(app-shell): defer CookieBanner render until first frame
    Converts AppShell to StatefulWidget and uses addPostFrameCallback to
    avoid rendering the banner before the first frame is complete.

--- lib/presentation/widgets/app_shell.dart
+++ lib/presentation/widgets/app_shell.dart
@@ -2,15 +2,33 @@ import 'package:flutter/material.dart';
 
 import 'consent/cookie_banner.dart';
 
-class AppShell extends StatelessWidget {
+class AppShell extends StatefulWidget {
   const AppShell({super.key, required this.child});
 
   final Widget? child;
 
   @override
+  State<AppShell> createState() => _AppShellState();
+}
+
+class _AppShellState extends State<AppShell> {
+  bool _firstFrameDone = false;
+
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      if (mounted) setState(() => _firstFrameDone = true);
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Stack(
-      children: [child ?? const SizedBox.shrink(), const CookieBanner()],
+      children: [
+        widget.child ?? const SizedBox.shrink(),
+        if (_firstFrameDone) const CookieBanner(),
+      ],
     );
   }
 }
